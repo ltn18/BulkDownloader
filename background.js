50e89d9e2af9d739c17ab20437eb29d82d546c16
HOANGLAM2952002 <tommy2952002@gmail.com>
2021-05-21 12:14:37 +0700

    add filename that includes search query for downloading

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+  console.log(req.searchTitle);
+
   if (req.action === "downloads") {
     for (let i = 0; i < req.data.length; i++) {
-      chrome.downloads.download({url: req.data[i].src})
+      var filename = `${req.searchTitle}_${i}`;
+      var url = req.data[i].src;
+      chrome.downloads.download({ url: url, saveAs: true, filename: filename });
     }
 
     sendResponse("Done");
   }
-})
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,16 @@
 var images = [];
+var title = "";
+
+function getSearchTitle(url) {
+  var s = url.search("q=");
+  var e = url.search("&");
+  console.log(url.substring(s+2, e));
+  return url.substring(s+2, e);
+}
 
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  title = getSearchTitle(tabs[0].url);
+
   chrome.tabs.sendMessage(tabs[0].id, { action: "get_images" }, (response) => {
     console.log("res: ", response);
 
@@ -18,7 +28,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 
 $(document).on("click", "#download_all", (e) => {
   console.log("Downloading all ...");
-  chrome.runtime.sendMessage({ action: "downloads", data: images }, (res) => {
+  chrome.runtime.sendMessage({ action: "downloads", data: images, searchTitle: title }, (res) => {
     console.log("Download complete!");
   });
 });
