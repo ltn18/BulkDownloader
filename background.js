d9f7720e611d17bf4a0a88ec32603aad83331cae
HOANGLAM2952002 <tommy2952002@gmail.com>
2021-05-21 12:31:40 +0700

    add images downloaded from query search apple

--- background.js
+++ background.js
@@ -3,9 +3,9 @@ chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
 
   if (req.action === "downloads") {
     for (let i = 0; i < req.data.length; i++) {
-      var filename = `${req.searchTitle}_${i}`;
+      var filename = `${req.searchTitle}_${i}.png`;
       var url = req.data[i].src;
-      chrome.downloads.download({ url: url, saveAs: true, filename: filename });
+      chrome.downloads.download({ url: url, filename });
     }
 
     sendResponse("Done");
